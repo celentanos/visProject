9c13b0478defe31f4f3fff06be19b0d78681e974
alisnitzki <andrej.lisnitzki.1@stud.htwk-leipzig.de>
2016-08-25 19:17:06 +0200

    in vector ändern

--- project/proj_Isolines.cpp
+++ project/proj_Isolines.cpp
@@ -57,37 +57,37 @@ public:
         double **d = new double*[ny];
         for (size_t i = 0; i < ny; ++i)
             d[i] = new double[nx];
-        for (int y = ny - 1; y >= 0; --y)
+        for (int y = 0; y < ny; ++y)
             for (size_t x = 0; x < nx; ++x)
                 d[y][x] = field->values()[y * nx + x][0];
         debugLog() << "value1: " << field->values()[7120][0] << endl;
 
-        double *jub = new double[nx];         //x - jub
+        double *x = new double[nx];         //x - jub
         for (size_t i = 0; i < nx; ++i)
             grid->points()[i][0];
         debugLog() << "x1: " << grid->points()[0][0] << endl;
 
-        double *iub = new double[ny];         //y - iub
-        for (int i = ny - 1; i >= 0; --i)
+        double *y = new double[ny];         //y - iub
+        for (int i = 0; i < ny; ++i)
             grid->points()[i * nx][1];
         debugLog() << "y1: " << grid->points()[(ny - 1) * nx][1] << endl;
 
-        double z[1] = {10};
+        double z[1] = {1};
 
         /// Lines ##############################################################
-        vector<Vector3> v(2);
+        vector<Vector3> v;
 //        v[0] = Point3(45, 6, 0);
 //        v[1] = Point3(55, 16, 0);
 
-        conrec(d, 0, ny - 1, 0, nx - 1, jub, iub, 1, z, v);
+        conrec(d, 0, ny - 1, 0, nx - 1, y, x, 1, z, v);
         debugLog() << "conrec: fertig" << endl;
 
         isolines->add(Primitive::LINES).setColor(Color(1, 0, 0)).setVertices(v);
         // Aufräumen -----------------------------------------------------------
-        for (size_t i = 0; i < ny; ++i)
-            delete d[i];
-        delete jub;
-        delete iub;
+//        for (size_t i = 0; i < ny; ++i)
+//            delete d[i];
+//        delete x;
+//        delete y;
     }
 };
 
